19e77c9ce5374f53223b5f77f276ed94d08c33ae
master <kujira5go5@gmail.com>
2018-08-16 12:42:48 +0900

    cursol move ok

--- test.py
+++ test.py
@@ -3,87 +3,139 @@
 import pygame
 from pygame.locals import *
 import sys
-
-pygame.init()
-
-SCR_WIDTH, SCR_HEIGHT = 320,160
-
-def load_pict(file_path, transparent_color=None):
-    try:
-        pict = pygame.image.load(file_path)
-
-    except pygame.error as message:
-        print("picture is missing:", file_path)
-        raise SystemExit(message)
-
-    pict = pict.convert()
-    if transparent_color is not None:
-        if transparent_color is -1:
-            transparent_color = pict.get_at((0, 0)) # 左上が透過色になる
-        pict.set_colorkey(transparent_color, pygame.RLEACCEL)
-
-    return pict, pict.get_rect()
+import math
 
 
-screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+#SCR_WIDTH, SCR_HEIGHT = 320, 160
+#screen = pygame.display.set_mode((SCR_WIDTH, SCR_HEIGHT))
+
+SCR_RECT = Rect(0, 0, 320, 160)
 
 pygame.display.set_caption(u"Window")
 
-jikiImg,jikiRect = load_pict("./pict/jiki.png", transparent_color=-1)
+#jikiImg, jikiRect = load_pict("./pict/jiki.png", transparent_color=-1)
 
 jiki_cur_Position = (0, 0)
 jiki_Position = []
 
-
-
 vx = vy = 1.5
-clock = pygame.time.Clock()
-jikiRect.left, jikiRect.top = 16, 30# 初期位置
-while True:
-    clock.tick(60)
-    screen.fill((50, 50, 50))  # 画面を青色で塗りつぶす
-    pygame.draw.circle(screen, (255, 100, 100), (150, 120), 5)
-    jikiRect.move_ip(vx, vy)
 
-    if jikiRect.left < 5 or jikiRect.right > 160:
-        vx = -vx
-    if jikiRect.top < 5 or jikiRect.bottom > 155:
-        vy = -vy
 
-    pressed_keys = pygame.key.get_pressed()
+START = (300, 40)
 
-    if pressed_keys[K_LEFT]:
-        jikiRect.move_ip(-vx, 0)
-    if pressed_keys[K_RIGHT]:
-        jikiRect.move_ip(vx, 0)
-    if pressed_keys[K_UP]:
-        jikiRect.move_ip(-vy, 0)
-    if pressed_keys[K_DOWN]:
-        jikiRect.move_ip(vy, 0)
 
-    mouse_pressed = pygame.mouse.get_pressed()
-    if mouse_pressed[0]:
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(SCR_RECT.size)
+
+    all = pygame.sprite.RenderUpdates()
+    Fireball.containers = all
+    Fireball.image = load_image("./fireball.png")
+
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(60)
+        screen.fill((50, 50, 50))
+
+        pygame.draw.circle(screen, (255, 100, 100), (150, 120), 5)
+
+        """自機の移動
+        jikiRect.move_ip(vx, vy)
+        if jikiRect.left < 5 or jikiRect.right > 160:
+            vx = -vx
+        if jikiRect.top < 5 or jikiRect.bottom > 155:
+            vy = -vy
+        
+
+        pressed_keys = pygame.key.get_pressed()
+
+        if pressed_keys[K_LEFT]:
+            jikiRect.move_ip(-vx, 0)
+        if pressed_keys[K_RIGHT]:
+            jikiRect.move_ip(vx, 0)
+        if pressed_keys[K_UP]:
+            jikiRect.move_ip(0, -vy)
+        if pressed_keys[K_DOWN]:
+            jikiRect.move_ip(0, vy)
+
+        mouse_pressed = pygame.mouse.get_pressed()
+        if mouse_pressed[0]:
+            x, y = pygame.mouse.get_pos()
+            x -= jikiImg.get_width() / 2
+            y -= jikiImg.get_height() / 2
+            jiki_Position.append((x, y))
+
         x, y = pygame.mouse.get_pos()
         x -= jikiImg.get_width() / 2
         y -= jikiImg.get_height() / 2
-        jiki_Position.append((x, y))
+        jiki_cur_Position = x, y
+        
+        for i, j in jiki_Position:
+            screen.blit(jikiImg, (i, j))
 
-    x, y = pygame.mouse.get_pos()
-    x -= jikiImg.get_width() / 2
-    y -= jikiImg.get_height() / 2
-    jiki_cur_Position = x, y
+        screen.blit(jikiImg, jikiRect)
+        """
 
-    for i, j in jiki_Position:
-        screen.blit(jikiImg, (i, j))
+        mouse_handler()
 
-    screen.blit(jikiImg, jikiRect)
-    pygame.display.update()  # 画面を更新
-    # イベント処理
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # 終了イベント
-            sys.exit()
+        all.apdate()
+        all.draw(screen)
 
 
+        pygame.display.update()  # 画面を更新
+        # イベント処理
+
+        for event in pygame.event.get():
+            if event.type == QUIT:  # 終了イベント
+                sys.exit()
+            if event.type == KEYDOWN and event.key == K_ESCAPE:
+                pygame.quit()
+                sys.exit()
 
 
+def mouse_handler():
+    mouse_pressed = pygame.mouse.get_pressed()
+    if mouse_pressed[0]:
+        x, y = pygame.mouse.get_pos()
+        Fireball(START, (x, y))
+
+
+def load_image(filename, colorkey=None):
+    try:
+        image = pygame.image.load(filename)
+    except pygame.error as message:
+        print("cant load image:", filename)
+        raise SystemExit(message)
+
+    image = image.convert()
+    if colorkey is not None:
+        if colorkey is -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey, RLEACCEL)
+    return image
+
+
+class Fireball(pygame.sprite.Sprite):
+    speed = 10
+
+    def __init__(self, start, target):
+        pygame.sprite.Sprite.__init__(self, self.containes)
+        self.rect = self.image.get_rect()
+        self.start = start
+        self.target = target
+        self.rect.center = self.start
+
+        self.direction = math.atan2(target[1]-start[1], target[0]-start[0])
+
+        self.vx = math.cos(self.direction) * self.speed
+        self.vy = math.sin(self.direction) * self.speed
+
+    def update(self):
+        self.rect.move_ip(self.vx, self.vy)
+
+        if not SCR_RECT.contains(self.rect):
+            self.kill()
+
+
+if __name__ == "__main__":
+    main()
